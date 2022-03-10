0bd12c712321e683ba72b3f34789be24ffcabf2e
Guilherme Duarte <62967603+GuilhermeDuarte14511@users.noreply.github.com>
2022-03-10 18:19:02 -0300

    Adição do botão Mostrar senha, e pagina de cadastro

---
+++ Projeto_Barbearia/wwwroot/js/sitejquery.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+	//Mostrar Senha
+	$(function () {
+		$('.js-block-view-password').each(function () {
+			var $btnPassword = $(".js-btn-password", this);
+			var $iconPassword = $(".js-icon-password", this);
+			var $inputPassword = $(".js-input-password", this);
+			$btnPassword.click(function () {
+				if ($inputPassword.attr('type') === 'password') {
+					$iconPassword.removeClass("fa-eye-slash").addClass("fa-eye");
+					$inputPassword.attr('type', 'text');
+				} else {
+					$iconPassword.removeClass("fa-eye").addClass("fa-eye-slash");
+					$inputPassword.attr('type', 'password');
+				}
+			});
+
+		});
+	});
+});
